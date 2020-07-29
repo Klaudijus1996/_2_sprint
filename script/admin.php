aeb55301f630cd07f79332c5e4eefabee16dc1a3
Klaudijus <klaudijus.k1996@gmail.com>
2020-07-30 00:44:07 +0300

    updt

--- script/admin.php
+++ script/admin.php
@@ -1,3 +1,4 @@
+<?php declare(strict_types = 1); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +8,37 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <?php 
+        $servername = "localhost";
+        $username = "root";
+        $password = "mysql";
+        $dbname = 'esybes_ir_rysiai';
+        $word = 'role';
+        
+        $conn = mysqli_connect($servername, $username, $password, $dbname); // Create connection
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        echo "Connected successfully<br>";
+
+        $sql = "SELECT staffid, first_name, last_name, $word FROM staff";
+
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "id: " . $row["staffid"]. " - Name: " . $row["first_name"]. ", " . $row["last_name"].", ".$row[$word]. "<br>";
+                // TODO input table data
+            }
+        } else {
+            echo "0 results";
+        }
+
+        mysqli_close($conn);
+
+        
+    ?>
     <?php require_once('logout.php') ?>
 </body>
 </html>
